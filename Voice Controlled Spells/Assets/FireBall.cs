7064566414de62c94d7e71d02aa4f831e1936f1d
Danbot02 <danbot02@gmail.com>
2022-05-06 14:15:04 +1000

    Basic speech for Fire Ball
    Basic speech implementation, not very efficient nor is it fast in anyway how ever it does now recognize when the player says "Fire Ball" and will react accordingly.
    Spacebar key to do the same is still there.

--- Voice Controlled Spells/Assets/FireBall.cs
+++ Voice Controlled Spells/Assets/FireBall.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Windows.Speech;
 using UnityEngine;
+using System;
 
 public class FireBall : MonoBehaviour
 {
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> spells = new Dictionary<string, System.Action>();
+
     public GameObject ThisOb;
     public Rigidbody Player;
     public TrailRenderer Trail;
@@ -24,6 +30,28 @@ public class FireBall : MonoBehaviour
             Rg.velocity = new Vector3(10, 0, 0);
             Trail.enabled = true;
         }
+
+        if (this.name == "SpellCast")
+        {
+            spells.Add("Fire ball", Fireball);
+
+            keywordRecognizer = new KeywordRecognizer(spells.Keys.ToArray());
+            keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+            keywordRecognizer.Start();
+
+            Debug.Log("Keywords added");
+        }
+    }
+
+    private void Fireball()
+    {
+        GameObject ob = GameObject.Instantiate(ThisOb, Player.transform);
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        spells[speech.text].Invoke();
     }
 
     void Update()
@@ -31,8 +59,8 @@ public class FireBall : MonoBehaviour
         //If they space key is pressed and the objects name is SpellCast then copy this object with the same position the player has
         if (Input.GetKeyDown(KeyCode.Space) && this.name == "SpellCast")
         {
-            GameObject ob = GameObject.Instantiate(ThisOb, Player.transform);
-        }
+            Fireball();
+        }        
     }
 
     private void OnCollisionEnter(Collision collision)
